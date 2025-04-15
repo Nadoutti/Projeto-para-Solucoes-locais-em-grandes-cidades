64e9e72392361432fc67a03a624a0fd100180e86
Pedro Nadotti <nadottipedro5@gmail.com>
2025-04-15 12:20:37 -0300

    commitando algumas mudancas que fiz em live

---
+++ middleware/jwt.go
@@ -0,0 +1,8 @@
+package middleware
+
+import 
+
+
+func (routes map[string]bool)  {
+  
+}
--- routes/routes.go
+++ routes/routes.go
@@ -8,6 +8,14 @@ import (
 
 func SetUpRoutes() *gin.Engine {
   router := gin.Default()
+
+  publicRoutes := map[string]bool{
+    "POST, /api/login": true,
+    "POST, /api/register": true,
+    "POST, /api/forgot_password": true,
+    "POST, /api/validar_existencia": true,
+
+  }
   
 
   report := router.Group("/report")
